451cbe631bab55bef8d166495b15cd2aad2a8d49
insomniac235 <juric.benjamin23@gmail.com>
2019-04-03 00:19:42 +0200

    data fetch test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import reactfire from 'reactfire';
+
+const config = {
+  apiKey: "AIzaSyBlFEgp-x93QsnzdUB0J7G_h-pLk0yzoMo",
+  authDomain: "got-analytics.firebaseapp.com",
+  databaseURL: "https://got-analytics.firebaseio.com",
+  projectId: "got-analytics",
+  storageBucket: "got-analytics.appspot.com",
+  messagingSenderId: "953410490273"
+};
+firebase.initializeApp(config);
+
+class FirebaseApi {
+  votes = () => firebase.database().ref('votes');
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      votes: [],
+    };
+  }
+
+  componentWillMount() {  
+    this.votes = firebase.database().ref("votes");
+  }
+
+  componentDidMount() {
+    firebase.database().ref('votes').once('value').then(val => {
+      this.setState({votes: val.val()});
+    });
+  }
+
+  componentWillUnmount() {
+    this.votes.off();
+  };
+  
   render() {
+    const {votes} = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +51,14 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <div>
+            {
+              votes.length > 0 && 
+              <span>
+                {votes[votes.length - 1].picks}
+              </span>
+            }
+          </div>
           <a
             className="App-link"
             href="https://reactjs.org"
