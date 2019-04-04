2873aeb9eb4a29ca3456625d3446b5f620c19173
benjamin.juric <benjamin.juric@osb-ag.de>
2019-04-04 17:44:10 +0200

    added bootstrap and pushing data to database

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import firebase from 'firebase';
-import reactfire from 'reactfire';
 
 const config = {
   apiKey: "AIzaSyBlFEgp-x93QsnzdUB0J7G_h-pLk0yzoMo",
@@ -14,60 +12,46 @@ const config = {
 };
 firebase.initializeApp(config);
 
-class FirebaseApi {
-  votes = () => firebase.database().ref('votes');
-}
-
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      choices: [1,2,4,5,6,7],
       votes: [],
     };
   }
 
-  componentWillMount() {  
-    this.votes = firebase.database().ref("votes");
+  addChoice = (choice) => {
+    const votes = this.state.votes;
+    votes.push(choice);
+    this.setState({choices: this.state.choices, votes: votes});
   }
 
-  componentDidMount() {
-    firebase.database().ref('votes').once('value').then(val => {
-      this.setState({votes: val.val()});
+  submit = () => {
+    const votesRef = firebase.database().ref('votes');
+    const newEntryKey = votesRef.push().key;
+    votesRef.child(newEntryKey).set({
+      votes: this.state.votes,
+      timestamp: new Date().valueOf()
     });
   }
 
-  componentWillUnmount() {
-    this.votes.off();
-  };
-  
   render() {
-    const {votes} = this.state;
-
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <div>
-            {
-              votes.length > 0 && 
-              <span>
-                {votes[votes.length - 1].picks}
-              </span>
-            }
-          </div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="container">
+        <header className="d-flex">
+          {
+            this.state.choices.map(c => <button className="btn btn-outline-primary m-4 rounded-circle" key={c} onClick={(e) => this.addChoice(c)}>{c}</button>)
+          }
         </header>
+        <div className="d-flex my-5">
+          <strong>Picks: </strong>
+          {
+            this.state.votes.map(v => <span className="mx-3">{v}</span>)
+          }
+        </div>
+        <button className="btn btn-success" onClick={(e) => this.submit()}>SUBMIT</button>
       </div>
     );
   }
