f5d58abd636810a1c32e2de9d66d99a28f7602d0
Your Name <you@example.com>
2019-04-07 18:54:56 +0200

    Added rules accordion view

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,11 +1,90 @@
 import React, { Component } from "react";
+import '../styles/Rules.css';
+
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+// Demo styles, see 'Styles' section below for some notes on use.
+import 'react-accessible-accordion/dist/fancy-example.css';
 
 
 class Rules extends Component{
   render(){
     return (
-      <div>Rules are rules</div>
-    )
+      <Accordion>
+          <AccordionItem>
+              <AccordionItemHeading>
+                  <AccordionItemButton>
+                      General Rules
+                  </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel>
+                  <p>
+                    general rules stuff
+                  </p>
+              </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+              <AccordionItemHeading>
+                  <AccordionItemButton>
+                      Deadpool Rules
+                  </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel>
+                  <p>
+                    Select as many characters as you wish that you think will DIE in the next episode. For every correct prediction you are awarded 5 points. Beware, for every wrong pick you will be deducted 3 points.
+
+                  </p>
+              </AccordionItemPanel>
+          </AccordionItem>
+      <AccordionItem>
+          <AccordionItemHeading>
+              <AccordionItemButton>
+                  Iron Throne Rules
+              </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+              <p>
+                Select as many characters as you wish that you think will DIE in the next episode. For every correct prediction you are awarded 5 points. Beware, for every wrong pick you will be deducted 3 points.
+
+              </p>
+          </AccordionItemPanel>
+      </AccordionItem>
+  <AccordionItem>
+      <AccordionItemHeading>
+          <AccordionItemButton>
+              Azor Ahai Rules
+          </AccordionItemButton>
+      </AccordionItemHeading>
+      <AccordionItemPanel>
+          <p>
+            Select as many characters as you wish that you think will DIE in the next episode. For every correct prediction you are awarded 5 points. Beware, for every wrong pick you will be deducted 3 points.
+
+          </p>
+      </AccordionItemPanel>
+  </AccordionItem>
+  <AccordionItem>
+      <AccordionItemHeading>
+          <AccordionItemButton>
+              Special Chips Rules
+          </AccordionItemButton>
+      </AccordionItemHeading>
+      <AccordionItemPanel>
+          <p>
+            Special chips stuff 
+
+          </p>
+      </AccordionItemPanel>
+  </AccordionItem>
+</Accordion>
+
+
+    );
   }
 }
 
